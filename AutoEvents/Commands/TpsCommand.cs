c8be5f7edcfcfc43729059dd27aa28ae2df8c08a
noah <41436356+noahnightmare@users.noreply.github.com>
2024-04-10 21:59:45 +0100

    Add TPS command to the RA panel

---
+++ AutoEvents/Commands/TpsCommand.cs
@@ -0,0 +1,27 @@
+﻿using CommandSystem;
+using System;
+using Exiled.Permissions.Extensions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+
+namespace AutoEvents.Commands
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class TpsCommand : ICommand
+    {
+        public string Command { get; } = "tps";
+
+        public string[] Aliases { get; } = null;
+
+        public string Description { get; } = "Checks the TPS of the server";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            response = $"{(int)Server.Tps}/60";
+            return true;
+        }
+    }
+}
